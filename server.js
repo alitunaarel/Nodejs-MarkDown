a0b47a71a40a54cbc826047acfa38caa7ab0450e
ali <ali@iMac-de-Elodie.fritz.box>
2020-04-21 21:00:13 +0200

    my very first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require('mongoose')
+const articleRouter = require("./routes/articles");
+const app = express();
+
+
+mongoose.connect('mongodb://localhost/blogg', {
+    useNewUrlParser:true, useUnifiedTopology:true
+})
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended:false }))
+
+
+app.get("/", (req, res) => {
+  const articles = [
+    {
+      title: "Test Article",
+      createdAt: new Date(),
+      description: "Test Description"
+    },{
+        title: "Test Article 2",
+        createdAt: new Date(),
+        description: "Test Description 2"
+      }
+  ];
+  res.render("articles/index", { articles: articles });
+});
+
+app.use("/articles", articleRouter);
+app.listen(5000);
+
+
+//32.10
